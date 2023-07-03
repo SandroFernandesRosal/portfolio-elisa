248368e7e4c7a47a9216b4fd5f46a3e83366e974
Sandro Fernandes <sandrofernandesrosal@gail.com>
2023-07-03 00:16:22 -0300

    correção de bug no background

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
-      <body className={`${roboto.variable} ${cookie.variable} w-[100vw]`}>
+      <body className={`${roboto.variable} ${cookie.variable}`}>
         <Providers>
           <Header />
           {children}
